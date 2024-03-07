52807ab2c7218499a039cca1a3f57ad0d6726668
Thomas Fenot <t.fenot@proxiad.com>
2024-03-07 22:26:20 +0100

    Init Project
    Started the project

---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.scss';
+import { AiFillGithub } from "react-icons/ai";
+
+const Main: React.FC = () => {
+ return (
+    <main>
+      <h2>Hey ! </h2>
+      <p>I'm Thomas, a young a passionate full-stack engineer</p>
+      <AiFillGithub/>
+    </main>
+ );
+};
+
+export default Main;
